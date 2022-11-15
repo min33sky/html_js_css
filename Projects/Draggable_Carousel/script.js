f224c5f7c456cdbb2c6525a9ab04dbd3b35a04d9
min33sky <min33sky@naver.com>
2022-11-15 11:16:05 +0900

    feat: comment update

--- Projects/Draggable_Carousel/script.js
+++ Projects/Draggable_Carousel/script.js
@@ -8,6 +8,9 @@ let prevPageX = 0;
 let prevScrollLeft = 0;
 let positionDiff = 0;
 
+/**
+ * 화살표 아이콘 보이기/숨기기
+ */
 function showHideIcons() {
   let scrollWidth = carousel.scrollWidth - carousel.clientWidth; //? 최대 스크롤 너비
   arrowIcons[0].style.display = carousel.scrollLeft > 0 ? 'block' : 'none';
@@ -17,7 +20,7 @@ function showHideIcons() {
 
 arrowIcons.forEach((icon) => {
   icon.addEventListener('click', () => {
-    let firstImgWidth = firstImg.clientWidth + 14; //? 이미지 너비 + margin
+    let firstImgWidth = firstImg.clientWidth + 14; //? 이미지 너비 + margin (14px)
     carousel.scrollLeft += icon.id === 'left' ? -firstImgWidth : firstImgWidth;
     setTimeout(() => showHideIcons(), 60);
   });
@@ -60,27 +63,36 @@ function dragStop() {
   autoSlide();
 }
 
+/**
+ * 슬라이드 자동으로 넘기기
+ */
 function autoSlide() {
-  // if there is no image left to scroll then return from here
+  //? 스크롤 할 이미지가 없을 경우 (가장 끝 부분)이면 함수 종료
   if (
     carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 ||
     carousel.scrollLeft <= 0
   )
     return;
 
+  //? 이동 거리 체크를 위해 절대값으로 변환
   positionDiff = Math.abs(positionDiff);
+
   let firstImgWidth = firstImg.clientWidth + 14; //? 이미지 너비 + margin (14px)
 
+  //? 현재 슬라이드의 남은 거리 체크
   let valDifference = firstImgWidth - positionDiff;
 
-  let skip = 0;
+  let skip = 0; //? 넘겨야 할 슬라이드 수
 
+  //? 슬라이드 하나의 범위를 넘겼을 경우 음수가 리턴되고
+  //? Skip 값을 슬라이드 하나당 1씩 증가시켜야 한다.
   if (valDifference < 0) {
     let absVal = Math.abs(valDifference);
     let quotient = Math.floor(absVal / firstImgWidth) + 1;
     skip = firstImgWidth * quotient;
   }
 
+  //? 슬라이드 영역의 33%를 넘겼을 경우 슬라이드를 넘긴다.
   if (carousel.scrollLeft > prevScrollLeft) {
     carousel.scrollLeft +=
       positionDiff > firstImgWidth / 3 ? skip + valDifference : -positionDiff;
