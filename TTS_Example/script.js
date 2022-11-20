fa2e70136931611ccbb4423c90ac94338b2780e5
min33sky <min33sky@naver.com>
2022-11-20 13:28:04 +0900

    feat: tts

---
+++ TTS_Example/script.js
@@ -0,0 +1,33 @@
+const synth = window.speechSynthesis;
+
+const inputForm = document.querySelector('form');
+const inputTxt = document.querySelector('input');
+const voiceSelect = document.querySelector('select');
+
+let voices;
+
+function loadVoices() {
+  voices = synth.getVoices();
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+    option.value = i;
+    voiceSelect.appendChild(option);
+  }
+}
+
+// in Google Chrome the voices are not ready on page load
+if ('onvoiceschanged' in synth) {
+  synth.onvoiceschanged = loadVoices;
+} else {
+  loadVoices();
+}
+
+inputForm.onsubmit = (event) => {
+  event.preventDefault();
+
+  const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+  utterThis.voice = voices[voiceSelect.value];
+  synth.speak(utterThis);
+  inputTxt.blur();
+};
