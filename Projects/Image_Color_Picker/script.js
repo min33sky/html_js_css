0a2e124c0f9a52f1092ec89a003f7da5f573c86b
min33sky <min33sky@naver.com>
2023-03-05 18:08:39 +0900

    fix: image-color-picker update

--- Projects/Image_Color_Picker/script.js
+++ Projects/Image_Color_Picker/script.js
@@ -13,10 +13,12 @@ const rgbButton = document.querySelector('#rgb-btn');
 let eyeDropper;
 let abortController;
 
+/**
+ * @description Check if the browser supports the EyeDropper API
+ * ? 현재 Chrome, Edge, Opera 정도만 지원
+ * ? https://developer.mozilla.org/en-US/docs/Web/API/EyeDropper
+ */
 function isExecutable() {
-  //? Check if the browser supports the API
-  //? 현재 Chrome, Edge, Opera 정도만 지원
-  //? https://developer.mozilla.org/en-US/docs/Web/API/EyeDropper
   if ('EyeDropper' in window) {
     pickColor.classList.remove('hide');
     eyeDropper = new EyeDropper();
@@ -28,43 +30,47 @@ function isExecutable() {
   }
 }
 
-function rgbToHex(r, g, b) {
-  return (
-    '#' + [r, g, b].map((x) => Number(x).toString(16).padStart(2, 0)).join('')
-  );
-}
-
+/**
+ * 색상 선택 핸들러
+ */
 async function colorSelector() {
   try {
     error.innerText = '';
 
+    //? 20초 안에 선택하지 않으면 경고 메시지
     abortController = new AbortController();
-    setTimeout(() => abortController.abort('10초 안에 선택해주세요 :)'), 10000);
+    setTimeout(() => abortController.abort('20초 안에 선택해주세요 :)'), 20000);
 
     const colorValue = await eyeDropper.open({
       signal: abortController.signal,
     });
 
-    const rgb = colorValue.sRGBHex;
-    const rgbArr = rgb
-      .replace(/[^0-9]/g, ' ')
-      .trim()
-      .split(' ')
-      .filter((n) => !!n);
-    const hex = rgbToHex(...rgbArr);
+    const hexValue = colorValue.sRGBHex;
+    const rgbArr = [];
+
+    //? hex 값을 rgb로 변환 (ex. #ffffff -> rgb(255, 255, 255)
+    for (let i = 1; i < hexValue.length; i += 2) {
+      rgbArr.push(parseInt(hexValue[i] + hexValue[i + 1], 16));
+    }
+
+    const rgbValue = `rgb(${rgbArr[0]}, ${rgbArr[1]}, ${rgbArr[2]})`;
 
     result.style.display = 'grid';
-    hexValRef.value = hex;
-    hexButton.dataset.val = hex;
-    rgbValRef.value = rgb;
-    rgbButton.dataset.val = rgb;
-    pickedColorRef.style.backgroundColor = rgb;
+    hexValRef.value = hexValue;
+    hexButton.dataset.val = hexValue;
+    rgbValRef.value = rgbValue;
+    rgbButton.dataset.val = rgbValue;
+    pickedColorRef.style.backgroundColor = rgbValue;
   } catch (err) {
     error.classList.remove('hide');
     error.innerText = err;
   }
 }
 
+/**
+ * 이미지 선택 핸들러
+ * - 이미지 미리보기 설정
+ */
 function selectImageHandler(e) {
   result.style.display = 'none';
   const file = e.target.files[0];
@@ -72,6 +78,9 @@ function selectImageHandler(e) {
   image.setAttribute('src', previewImage);
 }
 
+/**
+ * 클립보드에 복사
+ */
 function copyToClipboard(e) {
   const target = e.target.parentNode;
   const text = target.dataset.val;
